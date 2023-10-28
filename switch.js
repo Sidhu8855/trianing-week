152e7cade2c75601706ea9635cf2a90e34e29b1f
siddesh lonari <siddeshlonari01@gmail.com>
2023-10-28 22:53:28 +0530

    first commit

---
+++ switch.js
@@ -0,0 +1,24 @@
+var Day = "monday";
+var s = "start form work";
+
+switch (Day) {
+    case "Sunday":
+        console.log("Today is Sunday");
+        break;
+
+    case "tusday":
+        console.log("Today is tusday");
+        break;
+
+    case "monday":
+        console.log("Today is monday" + s);
+        break;
+
+    case "friday":
+        console.log("Today is friday");
+        break;
+
+    default:
+        console.log("I am default");
+        break;
+}
